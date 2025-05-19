2cd060126a4c7499e6d1131b558b215ea856a35a
Florea Andrei <florea.andrei20@yahoo.com>
2025-05-19 11:52:53 +0300

    Lab9

--- Laborator8/src/main/java/org/example/Main.java
+++ Laborator8/src/main/java/org/example/Main.java
@@ -5,25 +5,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
-        try {
-            var continents = new ContinentDAO();
-            continents.create("Europe");
-            Database.getConnection().commit();
-
-            var countries = new CountryDAO();
-            int europeId = continents.findByName("Europe");
-            countries.create("Romania", europeId);
-            countries.create("Ukraine", europeId);
-            Database.getConnection().commit();
-
-            ArrayList<String> europeanCountries = countries.findByContinent(europeId);
-            System.out.println("Countries in Europe:");
-            europeanCountries.forEach(System.out::println);
-
-            Database.closeConnection();
+        /*
+        String filePath = Main.class.getClassLoader().getResource("concap.csv").getPath();
+        CSVImporter importer = new CSVImporter();
+        importer.importData(filePath);
+        */
+        try{
+            CityDAO cityDAO = new CityDAO();
+            ArrayList<City> cities = cityDAO.findAll();
+            for(int i=0;i<cities.size();i++){
+                for(int j=i+1;j<cities.size();j++){
+                    City city1 =cities.get(i);
+                    City city2 =cities.get(j);
+                    double distance = DistanceCalculator.haversine(city1.getLatitude(), city1.getLongitude(),
+                            city2.getLatitude(), city2.getLongitude());
+                    System.out.printf("Distance between %s and %s: %.2f km%n",
+                            city1.getName(), city2.getName(), distance);
+                }
+            }
         } catch (SQLException e) {
-            System.err.println(e);
-            Database.rollback();
+            e.printStackTrace();
         }
     }
 }
